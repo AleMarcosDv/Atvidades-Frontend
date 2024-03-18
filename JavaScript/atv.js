ee0cff78a6dc67e4ea949e9f79d26b9833c80b05
Ale <Alex09112004marcos@gmail.com>
2024-03-18 17:30:06 -0300

    Add nv atv

--- JavaScript/atv.js
+++ JavaScript/atv.js
@@ -1,14 +1,16 @@
-let numerosDaSorte = [37, 14, 26, 5, 94, 87];
+// let numerosDaSorte = [37, 14, 26, 5, 94, 87];
 
  
-for(let i = 0; i <= numerosDaSorte.length; i++){
- let num = numerosDaSorte[i];
+// for(let i = 0; i <= numerosDaSorte.length; i++){
+//  let num = numerosDaSorte[i];
 
-   if (num %2 === 0 && num < 50){
-      console.log(num + " O número é par e menor que 50");
-    }else if (num < 50){
-      console.log(num + " O número é menor que 50");
-    }else{
-      console.log(num + " O número é maior que 50");
-    }
-}
+//    if (num %2 === 0 && num < 50){
+//       console.log(num + " O número é par e menor que 50");
+//     }else if (num < 50){
+//       console.log(num + " O número é menor que 50");
+//     }else{
+//       console.log(num + " O número é maior que 50");
+//     }
+// }
+
+console.log("Conexão foi feita com SUCESSO!!")

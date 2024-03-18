0e523cf57a8ab48023e42f95a91423c768cda22e
Ale <Alex09112004marcos@gmail.com>
2024-03-18 15:31:44 -0300

    Add JS

---
+++ JavaScript/afk.js
@@ -0,0 +1,14 @@
+let numerosDaSorte = [37, 14, 26, 5, 94, 87];
+
+ 
+for(let i = 0; i <= numerosDaSorte.length; i++){
+ let num = numerosDaSorte[i];
+
+   if (num %2 === 0 && num < 50){
+      console.log(num + " O número é par e menor que 50");
+    }else if (num < 50){
+      console.log(num + " O número é menor que 50");
+    }else{
+      console.log(num + " O número é maior que 50");
+    }
+}

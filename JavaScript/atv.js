c7ac13cb91dd1344d8a72b8ba09940178fe92b05
Ale <Alex09112004marcos@gmail.com>
2024-03-18 19:34:53 -0300

    Add nv atv3

--- JavaScript/atv.js
+++ JavaScript/atv.js
@@ -1,3 +1,4 @@
+//atvdd1
 // let numerosDaSorte = [37, 14, 26, 5, 94, 87];
 
  
@@ -13,4 +14,23 @@
 //     }
 // }
 
-console.log("Conexão foi feita com SUCESSO!!")
+//atvdd2
+//console.log("Conexão foi feita com SUCESSO!!")
+
+//atvdd3
+let h1txt = document.getElementById("titulo");
+let atxt = document.querySelectorAll(".link") 
+let oladd=document.querySelector("#lista-ordenada")
+
+h1txt.innerText = "ProzEdu";
+atxt[0].innerText="ProzEdu";
+atxt[1].innerText="Facebook"
+
+oladd.innerHTML =`
+<li> Hello world!!!, I'm tired of saying this</li>
+<li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem</li>
+<li>doloremque laudantium, totam rem aperiam, eaque ipsa</li>
+<li>et quasi architecto beatae vitae dicta sunt explicabo</li>
+<li>sed quia non numquam eius modi tempora incidunt ut labore et</li>
+<li>Quis autem vel eum iure reprehenderit qui in ea voluptate </li>
+`
